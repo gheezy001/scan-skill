f8a297524a496848e4ec35bb581c98f548b6e060
gheezy001 <gdjimrabaye1@gmail.com>
2026-06-30 20:01:26 +0000

    docs: add root README

--- sprint2/sprint2/backend/src/mode-operatoire/mode-operatoire.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ModeOperatoireController } from './mode-operatoire.controller';
-import { ModeOperatoireService } from './mode-operatoire.service';
-
-@Module({
-  controllers: [ModeOperatoireController],
-  providers: [ModeOperatoireService],
-})
-export class ModeOperatoireModule {}
